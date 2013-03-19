a9b7a52b49b8dfe725a2e759ca065d2d7354963b
Jason Kraus <zbyte64@gmail.com>
2013-03-19 12:45:47 -0700

    patterned out adminlink publisher

--- eventsocket/publishers/adminlink.py
+++ eventsocket/publishers/adminlink.py
@@ -1,24 +1,44 @@
 from eventsocket.publishers.base import Publisher
 
+
 class HyperadminLinkPublisher(Publisher):
     '''
     Submits the data to a hyperadmin link in the system
     '''
     
-    def __init__(self, site, endpoint, method='POST', **kwargs):
+    def __init__(self, site, endpoint, method='POST', url_args=[], url_kwargs={}, **kwargs):
         '''
         :param site: The django url name of the site
         :param endpoint: The url name of the endpoint
         :param method: The HTTP method to use
         '''
-        self.site = site
-        self.endpoint = endpoint
-        self.method = method
+        self.site_urlname = site
+        self.endpoint_urlname = endpoint
+        self.method = method.upper()
+        self.url_args = url_args
+        self.url_kwargs = url_kwargs
         super(HyperadminLinkPublisher, self).__init__(**kwargs)
     
     def publish(self, message):
-        #get endpoint
-        #get endpoint link
-        #?match message to link form params?
-        pass
+        endpoint = self.get_endpoint()
+        
+        params = {
+            'payload': {
+                'data':message #TODO deserialize
+                #ie parse_sql => MultiValueDict
+            },
+            'method': self.method,
+            'url_args': self.url_args,
+            'url_kwargs': self.url_kwargs,
+        }
+        #TODO this doesn't exist
+        response = endpoint.internal_dispatch(**params)
+        return response
+    
+    def get_site(self):
+        pass #TODO hyperadmin.get_api(self.site_urlname)
+    
+    def get_endpoint(self):
+        site = self.get_site()
+        return site.get_endpoint_from_urlname(self.endpoint_urlname)
 
